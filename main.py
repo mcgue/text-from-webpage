8b1b0dcb3aff340c9324539e8b5776860219f212
MC <mc8@protonmail.com>
2023-12-09 16:07:55 -0800

    add code to import beautifulsoup and get text from a page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Obtain list of five words with more than 8 letters from random wikipedia page
 
 import requests
+from bs4 import BeautifulSoup
 
 def get_url():
     #set up scraper for page
@@ -12,7 +13,7 @@ def get_url():
     if (response.status_code) == 200:
 
         # Return url
-        return response.url
+        return response
 
     # try later if not working
     else:
@@ -21,4 +22,12 @@ def get_url():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(get_url())
+    # set up scraper for page
+    response = requests.get(
+        url="https://en.wikipedia.org/wiki/Special:Random",
+    )
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.body.contents[0])
+    text = soup.get_text()
+    print(text)
